722d4f7c9c0fc296a4f93c582e8b3182fb964b1a
cseyve@free.fr <cseyve@free.fr@68c22a89-eaa0-f639-2c35-815395397992>
2011-05-17 05:10:57 +0000

    Renamed qmake-qt4 into qmake in build and install scripts

--- trunk/build.sh
+++ trunk/build.sh
@@ -3,10 +3,10 @@ echo "Building piaf..."
 cd main/
 
 echo " + building plugins library..."
-qmake-qt4 piaf-lib.pro && make $@ || echo "Build failed. Missing packages ? Please use ./prepare.sh to install needed packages for compilation (for ubuntu/debian)..."
+qmake piaf-lib.pro && make $@ || echo "Build failed. Missing packages ? Please use ./prepare.sh to install needed packages for compilation (for ubuntu/debian)..."
 
 echo " + building GUI..."
-qmake-qt4 piaf.pro && make $@ || echo "Build failed. Missing packages ? Please use ./prepare.sh to install needed packages for compilation (for ubuntu/debian)..."
+qmake piaf.pro && make $@ || echo "Build failed. Missing packages ? Please use ./prepare.sh to install needed packages for compilation (for ubuntu/debian)..."
 
 echo "Building plugins..."
 
