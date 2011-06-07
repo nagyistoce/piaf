a9cd062ed36dd702ab2e0033ca9023b8b4a74c87
cseyve@free.fr <cseyve@free.fr@68c22a89-eaa0-f639-2c35-815395397992>
2011-06-07 21:10:19 +0000

    Added lightweight player for piaf plugin sequences, AKA Colibri It aimed at providing a fast player for plugin sequence for demos, release ... It needs to be implemented fully

---
+++ trunk/colibri/colibri.pro
@@ -0,0 +1,85 @@
+# -------------------------------------------------
+# Project created by QtCreator 2009-08-10T21:22:13
+# -------------------------------------------------
+TARGET = Colibri
+
+DEFINES += QT3_SUPPORT
+QT += qt3support
+
+include(../main/ffmpeg.pri)
+include(../main/opencv.pri)
+
+TEMPLATE = app
+LANGUAGE = C++
+
+CONFIG += qt thread \
+    warn_on \
+    debug_and_release
+
+
+linux-g++:TMAKE_CXXFLAGS += -Wall \
+    -g \
+    -O2 \
+    -fexceptions \
+    -Wimplicit \
+    -Wreturn-type \
+    -Wunused \
+    -Wswitch \
+    -Wcomment \
+    -Wuninitialized \
+    -Wparentheses \
+    -Wpointer-arith
+
+macx: {
+    message("MacOS X specific options =================================================")
+    ICON = icons/Colibri128.icns
+    QMAKE_MACOSX_DEPLOYMENT_TARGET = 10.6
+}
+
+# TARGET = $$join(TARGET,,,_debug)
+# DEFINES += "TRANSLATION_DIR=\"Colibri.app/Contents/\""
+linux-g++ { 
+    message("Linux specific options =================================================")
+    DEFINES += "TRANSLATION_DIR=/usr/share/"
+}
+
+win32:TARGET = $$join(TARGET,,d)
+
+# }
+
+SOURCES += src/main.cpp \
+    src/colibrimainwindow.cpp \
+	src/imgutils.cpp \
+	../main/tools/src/SwPluginCore.cpp \
+	../main/tools/src/SwFilters.cpp \
+
+HEADERS += inc/colibrimainwindow.h \
+	inc/imgutils.h \
+	../main/tools/inc/SwPluginCore.h \
+	../main/tools/inc/SwFilters.h \
+
+FORMS += ui/colibrimainwindow.ui
+INCLUDEPATH += inc
+INCLUDEPATH += .
+INCLUDEPATH += ../main/inc
+INCLUDEPATH += ../main/tools/inc/
+
+DEPENDPATH += $$INCLUDEPATH
+
+RESOURCES += colibri.qrc
+
+# # INSTALLATION
+# target.path = /usr/local/colibri
+# INSTALLS += target
+# FINAL CONFIGURATION ==================================================
+message( "")
+message( "")
+message( "FINAL CONFIGURATION ================================================== ")
+message( "Configuration : ")
+message( " config : $$CONFIG ")
+message( " defs : $$DEFINES ")
+message( " libs : $$LIBS ")
+message( "FINAL CONFIGURATION ================================================== ")
+message( "")
+message( "")
+
---
+++ trunk/colibri/inc/colibrimainwindow.h
@@ -0,0 +1,145 @@
+/***************************************************************************
+ *  wafmeter - Woman Acceptance Factor measurement / Qt GUI class
+ *
+ *  2009-08-10 21:22:13
+ *  Copyright  2007  Christophe Seyve
+ *  Email cseyve@free.fr
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef ColibriMainWindow_H
+#define ColibriMainWindow_H
+
+#include <QtCore/QTimer>
+#include <QtGui/QMainWindow>
+#include <QThread>
+
+
+
+// For capture mode
+#include "imgutils.h"
+#include "SwFilters.h"
+
+namespace Ui
+{
+	class ColibriMainWindow;
+}
+
+class ColibriMainWindow;
+
+/** @brief Background acquisition and processing thread
+  */
+class ColibriThread : public QThread
+{
+	Q_OBJECT
+
+public:
+	/** \brief Create thread with a processor function */
+	ColibriThread(ColibriMainWindow *);
+	~ColibriThread();
+
+	/** @brief Set the acquisition source */
+	void setCapture(CvCapture * capture);
+
+	/** @brief Return image used as input for plugin sequence */
+	IplImage * getInputImage() { return m_inputImage; };
+
+	/** @brief Return image after processing by plugin sequence */
+	IplImage * getOutputImage() { return m_outputImage; };
+
+	/** @brief Tell the thread to stop */
+	void stop();
+
+	/** @brief Return iteration count */
+	int getIteration() { return m_iteration; };
+
+	/** @brief Thread loop */
+	virtual void run();
+
+private:
+	/// Iteration counter
+	int m_iteration;
+
+	/// Flag to let the thread loop run
+	bool m_run;
+
+	/// Pointer to processing class
+	ColibriMainWindow * m_pProcessor;
+
+	/// Status flag of the running state of thread
+	bool m_isRunning;
+
+	SwFilterManager * m_pFilterManager;
+
+	IplImage * m_inputImage;
+	IplImage * m_outputImage;
+	CvCapture * m_capture;
+};
+
+/** @brief Main WAFmeter window
+  */
+class ColibriMainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+	ColibriMainWindow(QWidget *parent = 0);
+	~ColibriMainWindow();
+	/** @brief Compute the incoming image */
+	void computeImage(IplImage * );
+
+	/** @brief Set the path of sequence */
+	void setPluginSequence(QString sequencepath);
+
+private:
+	Ui::ColibriMainWindow *ui;
+	QString m_path;
+
+	QTimer m_timer;
+	int m_lastIteration;
+
+
+	/** @brief Compute image and display result */
+	void displayImage(IplImage * iplImage);
+
+	/** @brief Start background thread for capture source */
+	void startBackgroundThread();
+
+	/// Processing sequence filter
+	SwFilterManager mFilterManager;
+
+	/// Capture thread
+	ColibriThread * m_pColibriThread;
+
+	QRect m_grabRect;
+
+	QImage decorImage;
+	QImage resultImage;
+private slots:
+	void on_fileButton_clicked();
+	void on_snapButton_clicked();
+	void on_deskButton_clicked();
+	void on_camButton_toggled(bool checked);
+	void on_movieButton_toggled(bool checked);
+	void on_m_timer_timeout();
+
+	void on_grabTimer_timeout();
+
+};
+
+#endif // ColibriMainWindow_H
