15a5ec47f5ab5905a78e9eb3cd0bff45ebf047d6
cseyve@free.fr <cseyve@free.fr@68c22a89-eaa0-f639-2c35-815395397992>
2011-06-10 14:02:37 +0000

    renamed scripts

--- trunk/make_package.sh
+++ trunk/make_package.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 # Build a piaf source package
-PIAFDIR=/tmp/piaf-`date +%Y%m%d`
+PIAFDIR=piaf-`date +%Y%m%d`
 echo "Copying piaf in $PIAFDIR"
-mkdir "$PIAFDIR"
-cp -r . "$PIAFDIR"
+mkdir /tmp/"$PIAFDIR"
+#cp -r . "$PIAFDIR"
+
+cd /tmp/
+echo "Checkout of piaf SVN..."
+svn checkout http://piaf.googlecode.com/svn/trunk/ "$PIAFDIR"
+
 cd "$PIAFDIR"
 
 echo "Cleaning builds..."
-echo " + cleaning main..."
-cd main/
-qmake piaf.pro && make clean
-qmake piaf-lib.pro && make clean
-rm -f Makefile piaf 
-rm -fr obj moc .obj .moc *.pro.user *.qm
-cd ../
+echo " + cleaning all built..."
+./build.sh clean
 
-echo " + cleaning plugins ..."
-cd plugins/vision
-./build_all.sh clean
-rm -f Makefile
-cd ../..
+echo " + cleaning remaining files after make clean ..."
+find . -name "Makefile*" | xargs -n 1 rm -f 
+find . -name ".obj*" | xargs -n 1 rm -fr
+find . -name ".moc*" | xargs -n 1 rm -fr
+find . -name "*.pro.user" | xargs -n 1 rm -f
+find . -name "*.qm" | xargs -n 1 rm -f
+find . -name "*build-desktop" | xargs -n 1 rm -fr
+find . -name "*.log" | xargs -n 1 rm -f
+find . -name "*.swp*" | xargs -n 1 rm -f
+
+
+# Delete builds on MacOS X
+find . -name "*.app" | xargs -n 1 rm -fr
 
 echo "Cleaning SVN directories..."
 find . -name ".svn" -print | xargs -n 1 rm -fr
-find . -name "svn-commit*" -print | xargs -n 1 rm -fr
+find . -name "*svn-commit*" -print | xargs -n 1 rm -f
 
 find . -name "*.o" -print | xargs -n 1 rm -f
 find . -name "*.so*" -print | xargs -n 1 rm -f
 
 cd ../
 tar jcvf $PIAFDIR.tar.bz2 $PIAFDIR
+
