667311aaa360354b11e06a71ba54b4c54656f877
cseyve@free.fr <cseyve@free.fr@68c22a89-eaa0-f639-2c35-815395397992>
2012-02-10 16:59:45 +0000

    copied the object for movie bmk form to workflow

---
+++ trunk/workflow/tools/inc/moviebookmarkform.h
@@ -0,0 +1,37 @@
+#ifndef MOVIEBOOKMARKFORM_H
+#define MOVIEBOOKMARKFORM_H
+
+#include <QWidget>
+#ifdef PIAF_LEGACY
+#include "videoplayertool.h"
+#endif
+#include "workshopmovie.h"
+
+namespace Ui {
+    class MovieBookmarkForm;
+}
+
+class MovieBookmarkForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MovieBookmarkForm(QWidget *parent = 0);
+    ~MovieBookmarkForm();
+
+	void setBookmarkList(QList<video_bookmark_t> list);
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::MovieBookmarkForm *ui;
+	QList<video_bookmark_t> m_listBookmarks;
+signals:
+	void signalNewBookmarkList(QList<video_bookmark_t>);
+	void signalExportSequence(QList<video_bookmark_t>);
+private slots:
+	void on_exportButton_clicked();
+	void on_delButton_clicked();
+};
+
+#endif // MOVIEBOOKMARKFORM_H
